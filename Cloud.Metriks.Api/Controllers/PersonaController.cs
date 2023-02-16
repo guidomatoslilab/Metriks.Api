213b84e63db79375bc1194eab700c10726ff3e42
Jose Cristobal <jose.cristobal@lilab.pe>
2023-02-16 16:04:41 -0500

    avance modificacion y adaptaicon de estructura inicial

---
+++ Cloud.Metriks.Api/Controllers/PersonaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud.Metriks.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[action]/{rut}")]
+        public ActionResult Buscar(string rut)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Cloud.Metriks.Api/Service/Persona/PersonaService.cs
@@ -0,0 +1,17 @@
+﻿using Cloud.Metriks.Api.Interface.Service.Persona;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud.Metriks.Api.Service.Persona
+{
+    public class PersonaService : IPersonaService
+    {
+        public void Buscar(string rut)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
